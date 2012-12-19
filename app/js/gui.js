2ef8a5c641977ed6907c2074e516f64562ba2bfd
ericso <services@ericso.me>
2012-12-19 13:52:09 -0800

    Added rudimentery searching

--- app/js/gui.js
+++ app/js/gui.js
@@ -1,39 +1,87 @@
+////////////////////////
+/// GLOBAL VARIABLES ///
+////////////////////////
+var queueSongs; // Songs that are in the queue
+var allSongs; // Song "database" to search from and add to
+var toAddSongs; // Songs from requests that aren't yet in the database
+var foundSongs; // Songs that are found from a search
+
+////////////////////////
+/// GUI MANIPULATION ///
+////////////////////////
 function setupApp() {
-	/*** Switching Tabs ***/
+	/*** Prepopulate allSongs array with some songs for testing ***/
+	allSongs = [{ artist: "Janelle Monae", title: "Many Moons", album: "Metropolis: The Chase Suite" },
+				{ artist: "Fun.", title: "Some Nights", album: "Some Nights" },
+				{ artist: "MIKA", title: "Love Today", album: "Life in Cartoon Motion" }
+	];
+
+	/*** Switching tabs event handlers ***/
 	$('#index_tab').click( function() {
-		// Find the currently active tab
+		// Find the currently active tab and switch the tab
 		var activeTab = $('#nav_bar .active');
-		
-		// Switch the tabs
 		switchTab(activeTab.attr('id'), 'index_tab');
 
 	});
 	$('#queue_tab').click( function() {
-		// Find the currently active tab
+		// Find the currently active tab and switch the tab
 		var activeTab = $('#nav_bar .active');
-		
-		// Switch the tabs
 		switchTab(activeTab.attr('id'), 'queue_tab');
 
 	});
 	$('#search_tab').click( function() {
-		// Find the currently active tab
+		// Find the currently active tab and switch the tab
 		var activeTab = $('#nav_bar .active');
-		
-		// Switch the tabs
 		switchTab(activeTab.attr('id'), 'search_tab');
 
 	});
 	$('#help_tab').click( function() {
-		// Find the currently active tab
+		// Find the currently active tab and switch the tab
 		var activeTab = $('#nav_bar .active');
-		
-		// Switch the tabs
 		switchTab(activeTab.attr('id'), 'help_tab');
 
 	});
+
+	/*** Searching for songs event handlers ***/
+	$('#song_search_button').click ( function() {
+		// Clear the foundSongs array
+		foundSongs = [];
+
+		// Empty the song list
+		$('#search_results_list').empty();
+
+		// Grab the text from the search box
+		var searchText = $('#song_search_input').val();
+
+		var found = false; // Flag set to true if a song is found
+		if (searchText) {
+			// Search the allSongs array for song
+			for (var i = 0; i < allSongs.length; i++) {
+				
+				if ((allSongs[i].artist.indexOf(searchText) >= 0) || (allSongs[i].title.indexOf(searchText) >= 0) || (allSongs[i].album.indexOf(searchText) >= 0)) {
+					found = true;
+					// alert(allSongs[i].artist);
+					// alert(allSongs[i].title);
+					// alert(allSongs[i].album);
+
+					// Push found song onto foundSongs array
+					foundSongs.push(allSongs[i]);
+				}
+			}
+		}
+
+		if (found) {
+			//alert(foundSongs.length);
+			for (var i = 0; i < foundSongs.length; i++) {
+				var divToAppend = $('<div class="well"><a class="pull-left" href="javascript:void(0)"><img class="thumbnail" src="assets/img/holder.png"></a><button id="' + 'song' + i + 'id' + '" type="submit" class="btn btn-primary pull-right">Request</button><div class="results-info"><h4>' + foundSongs[i].title + '<span> </span><small>' + foundSongs[i].artist + '</small></h4><p>' + foundSongs[i].album + '</p></div></div>');
+
+				$('#search_results_list').append(divToAppend);
+			}
+		}
+	});
 }
 
+// Handles switching the tabs
 function switchTab(fromTab, toTab) {
 	if (!$('#' + toTab).hasClass('disabled') && !$("#" + toTab).hasClass('active')) {
 		// Hide the currently active tab
---
+++ app/js/search.js
@@ -0,0 +1,16 @@
+// Event handler for the search button
+// $('#song_search_button').click ( function() {
+// 	// Grab the text from the search box
+// 	alert('clicked!');
+// 	var searchText = $('#song_search_input').val();
+// 	alert(searchText);
+// });
+
+
+// Search the all_songs array for song
+
+// If found, add song to queue and display notification of addition
+
+// If not found, add song to all_songs array and display notification
+// then add song to queue
+
